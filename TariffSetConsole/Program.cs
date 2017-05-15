d4d08a1bf2ac82d586325c1129f530129ad9436d
Austin Drenski <austin.drenski@gmail.com>
2017-05-15 00:32:45 -0400

    .

--- TariffSetConsole/Program.cs
+++ TariffSetConsole/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using AD.TariffSets;
 
@@ -19,10 +17,7 @@ namespace TariffSetConsole
 
             //const string directory = "\\Work\\Austin\\April 18 - new work";
             const string directory = "\\Users\\adren\\Desktop\\Argentina_V2";
-
-            Directory.EnumerateFileSystemEntries(Directory.GetCurrentDirectory()).ToList().ForEach(Console.WriteLine);
-            Directory.EnumerateFileSystemEntries(directory).ToList().ForEach(Console.WriteLine);
-
+            
             Task task =
                 TargetTariffYearFactory.Create(
                     $"{directory}\\Tariff data\\Downloads\\MFN_Applied_4_16_17.zip",
